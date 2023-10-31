14a5e9c1c219de6461ca7f6ec77c3e0eef58a070
emtabiraobarias <emtabirao@gmail.com>
2023-10-31 23:01:32 +1100

    Applied type safety in the module functions, along with cleaned comments on parameters and returns.

--- currencyconverter/helpers.py
+++ currencyconverter/helpers.py
@@ -1,20 +1,63 @@
 import pandas as pd
 import networkx as nx
 
-def read_csv_to_df(filepath, schema):
+def read_csv_to_df(filepath: str, schema: dict) -> pd.DataFrame:
+    '''
+    Returns a dataframe reading from csv file and applying datatype dictated by schema.
+
+    Parameters
+    ----------
+    filepath : str
+        File path to comma-delimited csv file
+    schema : dict
+        Dictionary describing columns and datatypes
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe with applied schema
+    '''
     cols = schema.keys()
     df = pd.read_csv(filepath, delimiter=',', header=None, names=cols, dtype=schema)
     return df
 
-# Create graph from df
-def translate_df_to_graph(df_data):
+def translate_df_to_graph(df_data: pd.DataFrame) -> nx.Graph:
+    '''
+    Generates and returns a graph from input DataFrame.
+
+    Parameters
+    ----------
+    df_data : pandas.DataFrame
+        Dataframe with applied schema of currencyconversiondata
+
+    Returns
+    -------
+    networkx.Graph
+        Directed graph
+    '''
     graph = nx.from_pandas_edgelist(df_data, 'FromCurrency', 'ToCurrency', 'Amount')
     # must be a directed graph given the one-way factual relationship between from-to currency
     graph = graph.to_directed()
     return graph
 
-# Query graph given start and end point
-def get_path(graph, source, target):
+def get_path(graph: nx.Graph, source: str, target: str) -> []:
+    '''
+    Query graph given start (source) and end (target) points
+
+    Parameters
+    ----------
+    graph : nx.Graph
+        Directed graph
+    source : str
+        3-character currency code.
+    target : str
+        3-character currency code.
+
+    Returns
+    -------
+    array
+        array list of nodes visited by the path
+    '''
     if(nx.has_path(graph, source, target)):
         path = nx.shortest_path(graph, source, target)
         return path
